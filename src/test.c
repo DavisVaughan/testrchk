83539740f4b38c0718a86687aeaa1213e12530e7
Davis Vaughan <davis@posit.co>
2025-04-08 08:44:45 -0400

    Confirm `SET_VECTOR_ELT` doesn't trigger error

--- src/test.c
+++ src/test.c
@@ -11,14 +11,21 @@ void r_list_poke(SEXP x, R_xlen_t i, SEXP value) {
 }
 
 SEXP ffi_make_list(void) {
+    // Allocate and protect `out` list
     SEXP out = PROTECT(Rf_allocVector(VECSXP, 1));
 
+    // Allocate element of the `out` list and immediately place it in the list,
+    // which protects it so we don't have to
+    // NOTE: rchk does not seem to think `elt` is protected here.
     SEXP elt = Rf_allocVector(INTSXP, 2);
-    r_list_poke(out, 0, elt);
+    // r_list_poke(out, 0, elt);
+    SET_VECTOR_ELT(out, 0, elt);
 
     SET_INTEGER_ELT(elt, 0, 1);
     SET_INTEGER_ELT(elt, 1, 2);
 
+    // Allocate `names` and set as the names of `elt`.
+    // NOTE: The allocation here is when rchk complains about `elt` being unprotected
     SEXP names = Rf_allocVector(STRSXP, 2);
     Rf_setAttrib(elt, R_NamesSymbol, names);
 
