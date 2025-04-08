a384fd98bb1bc19d17bf3933a6c623f95b4f6479
Davis Vaughan <davis@posit.co>
2025-04-08 09:04:02 -0400

    Force elt to be used

--- src/test.c
+++ src/test.c
@@ -10,28 +10,28 @@ void r_fancy_list_setter(SEXP x, R_xlen_t i, SEXP value) {
 }
 
 SEXP ffi_make_list_false_positive(void) {
-    SEXP out = PROTECT(Rf_allocVector(VECSXP, 2));
+    SEXP out = PROTECT(Rf_allocVector(VECSXP, 1));
 
     // NOTE: rchk does not seem to think `elt` is protected here.
     SEXP elt = Rf_allocVector(INTSXP, 2);
     r_fancy_list_setter(out, 0, elt);
 
     // NOTE: The allocation here is when rchk complains about `elt` being unprotected
-    SEXP elt2 = Rf_allocVector(INTSXP, 2);
-    r_fancy_list_setter(out, 1, elt2);
+    SEXP names = Rf_allocVector(STRSXP, 2);
+    Rf_setAttrib(elt, R_NamesSymbol, names);
 
     UNPROTECT(1);
     return out;
 }
 
 SEXP ffi_make_list_ok(void) {
-    SEXP out = PROTECT(Rf_allocVector(VECSXP, 2));
+    SEXP out = PROTECT(Rf_allocVector(VECSXP, 1));
 
     SEXP elt = Rf_allocVector(INTSXP, 2);
     SET_VECTOR_ELT(out, 0, elt);
 
-    SEXP elt2 = Rf_allocVector(INTSXP, 2);
-    SET_VECTOR_ELT(out, 1, elt2);
+    SEXP names = Rf_allocVector(STRSXP, 2);
+    Rf_setAttrib(elt, R_NamesSymbol, names);
 
     UNPROTECT(1);
     return out;
